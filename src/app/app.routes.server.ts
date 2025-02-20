8693f307b654e332b06b82064352e20d5ecfc572
AhmedAbdelmonem047 <ahmedabdelmonem047@gmail.com>
2025-02-20 22:37:24 +0300

    added payment

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -8,5 +8,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'productDetails/:productID',
     renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout/:cartId',
+    renderMode: RenderMode.Server
   }
 ];
