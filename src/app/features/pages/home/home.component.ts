03566c54fe2549917b34c51e71f249b81c9c7345
AhmedAbdelmonem047 <ahmedabdelmonem047@gmail.com>
2025-02-26 00:36:43 +0300

    added cart and wishlist extras in products component

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ProductsComponent } from "../products/products.component";
 import { HomeSliderComponent } from "../home-slider/home-slider.component";
 import { CategorySliderComponent } from "../category-slider/category-slider.component";
+import { ProductsComponent } from "../products/products.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ProductsComponent, HomeSliderComponent, CategorySliderComponent],
+  imports: [HomeSliderComponent, CategorySliderComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/features/pages/products/products.component.ts
+++ src/app/features/pages/products/products.component.ts
@@ -1,33 +1,26 @@
 import { WishlistService } from './../../../core/services/wishlist/wishlist.service';
 import { ProductsService } from './../../../core/services/products/products.service';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone } from '@angular/core';
 import { Products } from '../../../shared/interfaces/products/products';
-import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, NgIf, UpperCasePipe } from '@angular/common';
 import { FilterPipe } from "../../../shared/pipe/filter.pipe";
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../../core/services/cart/cart.service';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../../core/services/auth/auth.service';
-import { Cart } from '../../../shared/interfaces/cart/cart';
+import { Product } from '../../../shared/interfaces/wishlist/wishlist';
+
 
 @Component({
   selector: 'app-products',
-  imports: [CurrencyPipe, UpperCasePipe, FilterPipe, FormsModule, RouterLink],
+  imports: [CurrencyPipe, UpperCasePipe, FilterPipe, FormsModule, RouterLink, NgIf],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
-  constructor(private _ProductsService: ProductsService, private _CartService: CartService, private _AuthService: AuthService, private _WishlistService: WishlistService) { }
-
-  productList: Products[] = [];
-  searchVal: string = '';
-  isLoggedIn: boolean = false;
-  isLoading: boolean = false;
-  currentProductId: string = '';
-
-  ngOnInit(): void {
-    this.getAllProducts();
+  constructor(private _ProductsService: ProductsService, private _CartService: CartService, private _AuthService: AuthService,
+    private _WishlistService: WishlistService, private cdr: ChangeDetectorRef, private ngZone: NgZone) {
     this._AuthService?.userData.subscribe({
       next: (res) => {
         if (res !== null)
@@ -36,6 +29,37 @@ export class ProductsComponent {
           this.isLoggedIn = false
       }
     })
+  }
+
+  productList: Products[] = [];
+  searchVal: string = '';
+  isLoggedIn: boolean = false;
+  isLoading: boolean = false;
+  currentProductId: string = '';
+  cartList: any[] = [];
+  wishlistItems: Product[] = [];
+  totalItems: number = 0;
+
+  ngOnInit(): void {
+    this.getAllProducts();
+    if (this.isLoggedIn) {
+      this._CartService.getUserCart().subscribe({
+        next: (res) => {
+          this.cartList = res.data.products;
+        }
+      })
+      this._WishlistService.getUserWishlist().subscribe({
+        next: (res) => {
+          this.wishlistItems = res.data;
+        }
+      })
+    }
+  }
+
+  reloadComponent() {
+    this.ngZone.runOutsideAngular(() => {
+      location.reload();
+    });
   }
 
   getAllProducts() {
@@ -59,6 +83,8 @@ export class ProductsComponent {
     event.stopPropagation();
     this._CartService.addProductToCart(productID).subscribe({
       next: (res) => {
+        console.log(res);
+
         this._CartService.cartTotalItems.next(res.numOfCartItems);
         this.isLoading = false;
         Swal.fire({
@@ -79,6 +105,107 @@ export class ProductsComponent {
         console.log(err);
       }
     });
+    this.reloadComponent();
+    // this.cdr.detectChanges()
+  }
+
+  isProductInCart(productId: string) {
+    if (this.isLoggedIn) {
+      let flag = this.cartList.find(product => product.product._id === productId);
+      if (flag)
+        return true
+      else
+        return false
+    }
+    return false
+  }
+
+  getProductCount(productID: string) {
+    if (this.isLoggedIn) {
+      let flag = this.cartList.find(product => product.product._id === productID);
+      if (flag)
+        return flag.count
+      else
+        return 0
+    }
+    return 0
+  }
+
+  updateProductQuantity(event: Event, productId: string, count: number) {
+    event.stopPropagation();
+    if (count > 0) {
+      this.currentProductId = productId;
+      this.isLoading = true;
+      this._CartService.updateProductQuantity(productId, count).subscribe({
+        next: (res) => {
+          this.isLoading = false;
+          this.totalItems = res.numOfCartItems;
+          this.cartList = res.data.products;
+          this._CartService.cartTotalItems.next(res.numOfCartItems);
+          Swal.fire({
+            color: "#fff",
+            background: "#14803d",
+            position: "top-end",
+            icon: "success",
+            title: "Cart Updated Succesfully",
+            showCloseButton: true,
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            toast: true,
+          });
+          console.log(res);
+        },
+        error: (err) => {
+          this.isLoading = false;
+          console.log(err);
+        }
+      });
+    }
+    else {
+      this.removeSpecificItem(productId);
+    }
+    this.cdr.detectChanges();
+  }
+
+  removeSpecificItem(productId: string) {
+    this.currentProductId = productId;
+    this.isLoading = true;
+    this._CartService.removeSpecificItem(productId).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        this.totalItems = res.numOfCartItems;
+        this.cartList = res.data.products;
+        this._CartService.cartTotalItems.next(res.numOfCartItems);
+        Swal.fire({
+          color: "#fff",
+          background: "#14803d",
+          position: "top-end",
+          icon: "success",
+          title: "Cart Updated Succesfully",
+          showCloseButton: true,
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+          toast: true,
+        });
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log(err);
+      }
+    });
+  }
+
+  isProductInWishlist(productId: string) {
+    if (this.isLoggedIn) {
+      let flag = this.wishlistItems.find(product => product._id === productId);
+      if (flag)
+        return true
+      else
+        return false
+    }
+    return false
   }
 
   addToWishlist(event: Event, productID: string) {
@@ -103,5 +230,31 @@ export class ProductsComponent {
         console.log(err);
       }
     })
+    this.reloadComponent();
+  }
+
+  removeFromWishlist(event: Event, productID: string) {
+    event.stopPropagation();
+    console.log('hi', productID);
+    this._WishlistService.removeProductFromWishlist(productID).subscribe({
+      next: (res) => {
+        Swal.fire({
+          color: "#fff",
+          background: "#14803d",
+          position: "top-end",
+          icon: "success",
+          title: "Product Removed Succesfully",
+          showCloseButton: true,
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+          toast: true,
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+    this.reloadComponent();
   }
 }
