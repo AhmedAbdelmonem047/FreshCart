c34283d0f42b4fa8935676a4eabbb24b65020a08
AhmedAbdelmonem047 <ahmedabdelmonem047@gmail.com>
2025-03-04 15:32:26 +0300

    Ramadan Kareem

--- src/app/shared/components/lottie-animations/lottie-animations.component.ts
+++ src/app/shared/components/lottie-animations/lottie-animations.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule, isPlatformBrowser, NgIf } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import moment from 'moment-hijri';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
@@ -12,20 +13,20 @@ export class LottieAnimationsComponent {
 
   isBrowser!: boolean;
   isVisible: boolean = true;
-
-
+  
   constructor(@Inject(PLATFORM_ID) private platformId: object) {
     this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
   ngOnInit(): void {
     if (this.isBrowser) {
+      const hijriMonth = moment().iMonth() + 1;
       const hasSeenAnimation = sessionStorage.getItem('hasSeenAnimation');
 
-      if (hasSeenAnimation)
-        this.isVisible = false;
-      else
+      if (!hasSeenAnimation && hijriMonth === 9)
         sessionStorage.setItem('hasSeenAnimation', 'true');
+      else
+        this.isVisible = false;
 
       setTimeout(() => {
         this.isVisible = false;
