40d4b053f8aa437de041625f4be61364f196ded0
AhmedAbdelmonem047 <ahmedabdelmonem047@gmail.com>
2025-03-04 14:59:33 +0300

    Ramadan Kareem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./features/layout/navbar/navbar.component";
 import { FooterComponent } from "./features/layout/footer/footer.component";
 import { NgxSpinnerComponent } from 'ngx-spinner';
+import { LottieAnimationsComponent } from './shared/components/lottie-animations/lottie-animations.component';
+import { HomeComponent } from "./features/pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgxSpinnerComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgxSpinnerComponent, LottieAnimationsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/lottie-animations/lottie-animations.component.ts
+++ src/app/shared/components/lottie-animations/lottie-animations.component.ts
@@ -1,24 +1,41 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CommonModule, isPlatformBrowser, NgIf } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-lottie-animations',
-  imports: [CommonModule, LottieComponent],
+  imports: [CommonModule, LottieComponent, NgIf],
   templateUrl: './lottie-animations.component.html',
   styleUrl: './lottie-animations.component.scss'
 })
 export class LottieAnimationsComponent {
 
   isBrowser!: boolean;
+  isVisible: boolean = true;
 
 
   constructor(@Inject(PLATFORM_ID) private platformId: object) {
     this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
-  options: AnimationOptions = {
-    path: 'assets/images/ramadan.json', // Adjust to your Lottie file path
-  };
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      const hasSeenAnimation = sessionStorage.getItem('hasSeenAnimation');
 
+      if (hasSeenAnimation)
+        this.isVisible = false;
+      else
+        sessionStorage.setItem('hasSeenAnimation', 'true');
+
+      setTimeout(() => {
+        this.isVisible = false;
+      }, 3000);
+    }
+  }
+
+
+  options: AnimationOptions = {
+    path: 'assets/animations/ramadan.json',
+    loop: false,
+  };
 }
